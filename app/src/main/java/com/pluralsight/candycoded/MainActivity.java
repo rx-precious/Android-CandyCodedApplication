47fa3a3aa56e53883256878aaaadb58e99f04c71
Olajuwon Opeoluwa <opebash2012@gmail.com>
2019-10-26 10:53:08 +0100

    Revert "Revert "First commit alongside setting up infoIntent""
    This reverts commit 7183448e514d23abbab4a2f9d9ce36c407270650.

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,6 +82,19 @@ public class MainActivity extends AppCompatActivity {
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
+  @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+       // int id = item.getItemId();
+
+       // if(id == R.id.info){
+        Intent infoIntent = new Intent(this, InfoActivity.class);
+        startActivity(infoIntent);
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
 
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
